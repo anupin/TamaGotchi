67a794923d1ae6c36ab0034dd779838a0a2b73c0
Nipuna Perera <rukshannipuna12@gmail.com>
2017-06-06 20:39:10 +0200

    Initial commit

---
+++ app/src/main/java/com/anupin/tamagotchi/TamaMain.java
@@ -0,0 +1,26 @@
+package com.anupin.tamagotchi;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class TamaMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tama_main);
+
+        Button inizia=(Button) findViewById(R.id.button);
+
+        inizia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent Vmappa=new Intent(getApplicationContext(),TamaSelect.class);
+                startActivity(Vmappa);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/anupin/tamagotchi/TamaSelect.java
@@ -0,0 +1,13 @@
+package com.anupin.tamagotchi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TamaSelect extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tama_select);
+    }
+}
